424c09ad51624aa9d2391b14f933e752771a7b0c
quinnj <quinn.jacobd@gmail.com>
2019-06-20 09:53:35 -0600

    Updates

--- src/Arrays.jl
+++ src/Arrays.jl
@@ -2,142 +2,11 @@ module Arrays
 
 export Array
 
-export arraysize, arraylen, arrayref, arrayset
+const Buffer{T} = Base.Vector{T}
 
-Base.@pure isbitsunion(u) = u isa Union ? ccall(:jl_array_store_unboxed, Cint, (Any,), u) != Cint(0) : false
-
-Base.@pure function bitsunionsize(u::Union)
-    sz = Ref{Csize_t}(0)
-    algn = Ref{Csize_t}(0)
-    isunboxed = ccall(:jl_islayout_inline, Cint, (Any, Ptr{Csize_t}, Ptr{Csize_t}), u, sz, algn)
-    @assert isunboxed != Cint(0)
-    return sz[]
-end
-
-# isassigned
-function typelayout(::Type{T}) where {T}
-    sz = Ref{Csize_t}(0)
-    algn = Ref{Csize_t}(0)
-    isunboxed = ccall(:jl_islayout_inline, Cint, (Any, Ptr{Csize_t}, Ptr{Csize_t}), T, sz, algn)
-    return isunboxed != Cint(0), sz, algn
-end
-
-Base.@pure isunion(u) = u isa Union
-
-const LARGE_ALIGNMENT = 64
-const SMALL_ALIGNMENT = 16
-align(x, sz) = (x + sz - 1) & -sz
-
-zero!(ptr, n) = ccall(:memset, Cvoid, (Ptr{Cvoid}, Cint, Csize_t), ptr, 0, n)
-
-mutable struct Buffer{T} <: AbstractVector{T}
-    ptr::Ptr{T}
-    len::Int
-
-    Buffer{T}(ptr::Ptr{T}, len::Int) where {T} = new{T}(ptr, len)
-    function Buffer(::Type{T}, nelements::Int, alignment=LARGE_ALIGNMENT) where {T}
-        isunboxed, sz, algn = typelayout(T)
-        if isunboxed
-            # data stored in region
-            elsize = sizeof(T)
-            len = elsize * nelements
-            if isunion(T)
-                # extra "selector" byte stored for each isbits union element
-                len += nelements
-            elseif elsize == 1
-                # all byte buffers include extra null-terminating byte
-                len += 1
-            end
-        else
-            # only pointers to objects are stored
-            len = sizeof(Ptr{Cvoid}) * nelements
-        end
-        len = align(len, alignment)
-        ptr = convert(Ptr{T}, Libc.malloc(len))
-        if !isunboxed || isunion(T)
-            zero!(ptr, len)
-        end
-        if isunboxed && elsize == 1
-            unsafe_store!(ptr, 0, len)
-        end
-        buffer = new{T}(ptr, nelements)
-        finalizer(x->Libc.free(x.ptr), buffer)
-        return buffer
-    end
-end
-
-Base.size(b::Buffer) = (b.len,)
-
-Base.checkbounds(b::Buffer, i::Int) = i <= b.len || throw(BoundsError(b, i))
-
-Base.@pure nth_union(u, n) = u isa Union ? (n == 0 ? u.a : nth_union(u.b, n - 1)) : u
-
-typetagptr(b::Buffer{T}) = convert(Ptr{UInt8}, b.ptr + bitsunionsize(T) * b.len)
-
-function Base.getindex(b::Buffer{T}, i::Int) where {T}
-    @boundscheck checkbounds(b, i)
-    if isbitstype(T)
-        val = GC.@preserve b unsafe_load(b.ptr, i)
-        return val
-    elseif isbitsunion(T)
-        sel = unsafe_load(typetagptr(b), i)
-        val = GC.@preserve b unsafe_load(convert(Ptr{nth_union(T, sel)}, b.ptr + bitsunionsize(T) * (i - 1)))
-        return val
-    else
-        ptr = GC.@preserve b unsafe_load(convert(Ptr{Ptr{T}}, b.ptr), i)
-        if ptr == C_NULL
-            throw(UndefRefError())
-        end
-        val = GC.@preserve ptr unsafe_pointer_to_objref(ptr)
-        return val
-    end
-end
-
-Base.@pure find_union(u, ::Type{T}, i=0) where {T} = u isa Union ? (u.a === T ? i : find_union(u.b, T, i + 1)) : i
-
-function Base.setindex!(b::Buffer{T}, x::S, i::Int) where {T, S}
-    @boundscheck checkbounds(b, i)
-    if isbitstype(T)
-        GC.@preserve b unsafe_store!(b.ptr, x, i)
-    elseif isbitsunion(T)
-        GC.@preserve b unsafe_store!(convert(Ptr{S}, b.ptr + bitsunionsize(T) * (i - 1)), x)
-        sel = find_union(T, S)
-        GC.@preserve b unsafe_store!(typetagptr(b), sel, i)
-    else
-        # FIXME: how to tell the GC that the Buffer "owns" x now?
-        GC.@preserve x unsafe_store!(convert(Ptr{Ptr{T}}, b.ptr), pointer_from_objref(x), i)
-    end
-    return b
-end
-
-function bufferunset(b::Buffer{T}, i::Int) where {T}
-    @boundscheck checkbounds(b, i)
-    if !isbitstype(T) && !isbitsunion(T)
-        GC.@preserve b unsafe_store!(convert(Ptr{Ptr{Cvoid}}, b.ptr), C_NULL, i)
-    end
-    return b
-end
-
-function Base.isassigned(b::Buffer{T}, i::Int) where {T}
-    if i < 0 || i > b.len
-        return false
-    elseif !isbitstype(T) && !isbitsunion(T)
-        ptr = GC.@preserve b unsafe_load(convert(Ptr{Ptr{T}}, b.ptr), i)
-        return ptr != C_NULL
-    end
-    return true
-end
-
-Base.pointer(b::Buffer) = b.ptr
-
-function Base.copy(b::Buffer{T}) where {T}
-    newb = Buffer(T, b.len)
-    unsafe_copyto!(b.ptr, newb.ptr, b.len)
-    if isbitsunion(T)
-        unsafe_copyto!(typetagptr(b), typetagptr(newb), b.len)
-    end
-    return newb
-end
+Buffer{T}(len::Int) where {T} = Buffer{T}(undef, len)
+Buffer(ptr::Ptr{T}, len::Int; own::Bool=false) where {T} = Base.unsafe_wrap(Base.Array, ptr, len, own=own)
+# unsafe_copyto!(::Buffer{T}, doffs, ::Buffer{T}, soffs, n)
 
 # Array
 function _prod(dims::NTuple{N, Int}) where {N}
@@ -156,32 +25,20 @@ struct Array{T, N} <: AbstractArray{T, N}
 
     Array{T, N}(data::Buffer{T}, dims::NTuple{N, Int}, o::Integer, l::Integer) where {T, N} =
         new{T, N}(data, dims, UInt(o), UInt(l))
-    function Array(::Type{T}, dims::NTuple{N, Int}) where {T, N}
+    function Array{T, N}(dims::NTuple{N, Int}) where {T, N}
         len = _prod(dims)
-        data = Buffer(T, len)
+        data = Buffer{T}(len)
         A = new{T, N}(data, dims, 0, len)
         return A
     end
 end
 
-const Vector{T} = Array{T, 1}
-const Matrix{T} = Array{T, 2}
-
-Base.size(A::Array) = A.dims
-Base.IndexStyle(::Type{<:Array}) = Base.IndexLinear()
-
-Base.isassigned(A::Array, i::Int) = isassigned(A.data, i)
-Base.getindex(A::Vector, i::Int) = A.data[A.offset + i]
-Base.getindex(A::Array, i::Int) = A.data[i]
-Base.setindex!(A::Vector, x, i::Int) = setindex!(A.data, x, i)
-Base.setindex!(A::Array, x, i::Int) = setindex!(A.data, x, i)
-
 # constructors
 # type and dimensionality specified, accepting dims as series of Ints
-Array{T, 1}(::UndefInitializer, m::Int) where {T} = Array(T, (m,))
-Array{T, 2}(::UndefInitializer, m::Int, n::Int) where {T} = Array(T, (m, n))
-Array{T, 3}(::UndefInitializer, m::Int, n::Int, o::Int) where {T} = Array(T, (m, n, o))
-Array{T, N}(::UndefInitializer, d::Vararg{Int, N}) where {T, N} = Array(T, d)
+Array{T, 1}(::UndefInitializer, m::Int) where {T} = Array{T, 1}((m,))
+Array{T, 2}(::UndefInitializer, m::Int, n::Int) where {T} = Array{T, 2}((m, n))
+Array{T, 3}(::UndefInitializer, m::Int, n::Int, o::Int) where {T} = Array{T, 3}((m, n, o))
+Array{T, N}(::UndefInitializer, d::Vararg{Int, N}) where {T, N} = Array{T, N}(d)
 
 # type and dimensionality specified, accepting dims as tuples of Ints
 Array{T, 1}(::UndefInitializer, d::NTuple{1, Int}) where {T} = Array{T, 1}(undef, getfield(d, 1))
@@ -203,27 +60,99 @@ Array{T, 1}() where {T} = Array{T, 1}(undef, 0)
 Array(A::AbstractArray{T, N})    where {T, N}   = Array{T, N}(A)
 Array{T}(A::AbstractArray{S, N}) where {T, N, S} = Array{T, N}(A)
 
+# array.jl
+const Vector{T} = Array{T, 1}
+const Matrix{T} = Array{T, 2}
+const VecOrMat{T} = Union{Vector{T}, Matrix{T}}
+
+vect() = Vector{Any}()
+vect(X::T...) where {T} = T[ X[i] for i = 1:length(X) ]
+
+function vect(X...)
+    T = Base.promote_typeof(X...)
+    return copyto!(Vector{T}(undef, length(X)), X)
+end
+
+Base.size(A::Array) = A.dims
+Base.IndexStyle(::Type{<:Array}) = Base.IndexLinear()
+
+elsize(::Type{<:Array{T}}) where {T} = isbitstype(T) ? sizeof(T) : (Base.isbitsunion(T) ? Base.bitsunionsize(T) : sizeof(Ptr))
+Base.sizeof(a::Array) = Core.sizeof(a.data)
+
+Base.isassigned(A::Array, i::Int...) = isassigned(A.data, i...)
+Base.getindex(A::Vector, i::Int) = A.data[A.offset + i]
+Base.getindex(A::Array, i::Int) = A.data[i]
+Base.setindex!(A::Vector, x, i::Int) = setindex!(A.data, x, i)
+Base.setindex!(A::Array, x, i::Int) = setindex!(A.data, x, i)
+
 Base.pointer(A::Vector) = pointer(A.data) + A.offset
 Base.pointer(A::Array) = pointer(A.data)
 
-function Base.unsafe_wrap(::Type{Vector{UInt8}}, s::String)
-    len = sizeof(s)
-    data = Buffer{UInt8}(pointer(s), len)
-    return Vector{UInt8}(data, (len,), 0, len)
+Base.unsafe_copyto!(dest::Array{T}, doffs, src::Array{T}, soffs, n) where T =
+    unsafe_copyto!(dest.data, doffs + dest.offset, src.data, soffs + src.offset, n)
+
+function Base.copyto!(dest::Array{T}, doffs::Integer, src::Array{T}, soffs::Integer, n::Integer) where T
+    n == 0 && return dest
+    n > 0 || Base._throw_argerror()
+    if soffs < 1 || doffs < 1 || soffs+n-1 > length(src) || doffs+n-1 > length(dest)
+        throw(BoundsError())
+    end
+    unsafe_copyto!(dest, doffs, src, soffs, n)
+    return dest
 end
 
-function Base.unsafe_wrap(::Union{Type{Array}, Type{Array{T}}, Type{Array{T, N}}},
-                          ptr::Ptr{T}, dims::NTuple{N, Int}; own::Bool=false) where {T, N}
-    len = _prod(dims)
-    # TODO: handle own argument
-    data = Buffer{T}(ptr, len)
-    return Array{T, N}(data, dims, 0, len)
-end
-Base.unsafe_wrap(T::Type, p::Ptr, len::Int; own::Bool=false) = unsafe_wrap(T, p, (len,); own=own)
-# Base.unsafe_wrap(T::Type, p::Ptr, dims::NTuple{N,<:Integer}; own::Bool=false) where {N} =
-#     unsafe_wrap(T, p, convert(Tuple{Vararg{Int}}, dims), own=own)
+Base.copyto!(dest::Array{T}, src::Array{T}) where {T} = copyto!(dest, 1, src, 1, length(src))
 
 Base.copy(A::Array{T, N}) where {T, N} = Array{T, N}(copy(A.data), A.dims, A.offset, A.length)
+
+## Constructors ##
+
+Base.similar(a::Array{T,1}) where {T}                    = Vector{T}(undef, size(a,1))
+Base.similar(a::Array{T,2}) where {T}                    = Matrix{T}(undef, size(a,1), size(a,2))
+Base.similar(a::Array{T,1}, S::Type) where {T}           = Vector{S}(undef, size(a,1))
+Base.similar(a::Array{T,2}, S::Type) where {T}           = Matrix{S}(undef, size(a,1), size(a,2))
+Base.similar(a::Array{T}, m::Int) where {T}              = Vector{T}(undef, m)
+Base.similar(a::Array, T::Type, dims::Dims{N}) where {N} = Array{T,N}(undef, dims)
+Base.similar(a::Array{T}, dims::Dims{N}) where {T,N}     = Array{T,N}(undef, dims)
+
+function Base.getindex(::Type{T}, vals...) where T
+    a = Vector{T}(undef, length(vals))
+    @inbounds for i = 1:length(vals)
+        a[i] = vals[i]
+    end
+    return a
+end
+
+Base.getindex(::Type{T}) where {T} = (Base.@_inline_meta; Vector{T}())
+Base.getindex(::Type{T}, x) where {T} = (Base.@_inline_meta; a = Vector{T}(undef, 1); @inbounds a[1] = x; a)
+Base.getindex(::Type{T}, x, y) where {T} = (Base.@_inline_meta; a = Vector{T}(undef, 2); @inbounds (a[1] = x; a[2] = y); a)
+Base.getindex(::Type{T}, x, y, z) where {T} = (Base.@_inline_meta; a = Vector{T}(undef, 3); @inbounds (a[1] = x; a[2] = y; a[3] = z); a)
+
+function Base.getindex(::Type{Any}, @nospecialize vals...)
+    a = Vector{Any}(undef, length(vals))
+    @inbounds for i = 1:length(vals)
+        a[i] = vals[i]
+    end
+    return a
+end
+Base.getindex(::Type{Any}) = Vector{Any}()
+
+function unsafe_wrap(::Union{Type{Array}, Type{Array{T}}, Type{Array{T, N}}},
+                          ptr::Ptr{T}, dims::NTuple{N, Int}; own::Bool=false) where {T, N}
+    len = _prod(dims)
+    data = Buffer(ptr, len; own=own)
+    return Array{T, N}(data, dims, 0, len)
+end
+
+unsafe_wrap(T::Type, p::Ptr, len::Int; own::Bool=false) = unsafe_wrap(T, p, (len,); own=own)
+unsafe_wrap(T::Type, p::Ptr, dims::NTuple{N,<:Integer}; own::Bool=false) where {N} =
+    unsafe_wrap(T, p, convert(Tuple{Vararg{Int}}, dims), own=own)
+
+function unsafe_wrap(::Type{Vector{UInt8}}, s::String)
+    len = sizeof(s)
+    data = Buffer(pointer(s), len; own=true)
+    return Vector{UInt8}(data, (len,), 0, len)
+end
 
 Base.String(v::Vector{UInt8}) = unsafe_string(pointer(v), length(v))
 
